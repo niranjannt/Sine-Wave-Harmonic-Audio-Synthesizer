02b78a897b5db0d853059cb9e19a154024f686e2
Thompson Truong <thomtruong545@utexas.edu>
2025-10-04 23:08:17 -0500

    Fixed Array MACROS

--- sw/src/music.c
+++ sw/src/music.c
@@ -13,55 +13,55 @@
 #include "Timer1A.h"
     // write this// Period =  80000000/7272/Freq=11,001/Freq
     // write this// Period =  80000000/7272/Freq=11,001/Freq
-#define C7   (38222/64)  // 2093 Hz
-#define B6  (40490/64)   // 1975.5 Hz
+#define CC7   (38222/64)  // 2093 Hz
+#define BB6  (40490/64)   // 1975.5 Hz
 #define BF6  (42902/64)   // 1864.7 Hz
-#define A6  (45454/64)   // 1760 Hz
+#define AA6  (45454/64)   // 1760 Hz
 #define AF6  (48157/64)   // 1661.2 Hz
-#define G6   (51020/64)   // 1568 Hz
+#define GG6   (51020/64)   // 1568 Hz
 #define GF6  (54054/64)   // 1480 Hz
-#define F6   (57269/64)   // 1396.9 Hz
-#define E6   (60675/64)   // 1318.5 Hz
+#define FF6   (57269/64)   // 1396.9 Hz
+#define EE6   (60675/64)   // 1318.5 Hz
 #define EF6  (64282/64)   // 1244.5 Hz
-#define D6   (68102/64)  // 1174.7 Hz
+#define DD6   (68102/64)  // 1174.7 Hz
 #define DF6  (72156/64)  // 1108.7 Hz
-#define C6   (76445/64)   // 1046.5 Hz
-#define B5    (80988/64)  // 987.8 Hz
+#define CC6   (76445/64)   // 1046.5 Hz
+#define BB5    (80988/64)  // 987.8 Hz
 #define BF5   (85809/64)   // 932.3 Hz
-#define A5    (90909/64)   // 880 Hz
+#define AA5    (90909/64)   // 880 Hz
 #define AF5   (96315/64)   // 830.6 Hz
-#define G5    (102040/64)   // 784 Hz
+#define GG5    (102040/64)   // 784 Hz
 #define GF5  (108108/64)   // 740 Hz
-#define F5   (114531/64)   // 698.5 Hz
-#define E5   (121340/64)   // 659.3 Hz
+#define FF5   (114531/64)   // 698.5 Hz
+#define EE5   (121340/64)   // 659.3 Hz
 #define EF5  (128555/64)   // 622.3 Hz
-#define D5   (136216/64)   // 587.3 Hz
+#define DD5   (136216/64)   // 587.3 Hz
 #define DF5  (144300/64)   // 554.4 Hz
-#define C5  (152875/64)   // 523.3 Hz
-#define B4  (161976/64)   // 493.9 Hz
+#define CC5  (152875/64)   // 523.3 Hz
+#define BB4  (161976/64)   // 493.9 Hz
 #define BF4 (171600/64)   // 466.2 Hz
-#define A4  (181818/64)   // 440 Hz
+#define AA4  (181818/64)   // 440 Hz
 #define AF4 (192631/64)   // 415.3 Hz
-#define G4 (204081/64)   // 392 Hz
+#define GG4 (204081/64)   // 392 Hz
 #define GF4 (216216/64)   // 370 Hz
-#define F4  (229095/64)   // 349.2 Hz
-#define E4 (242718/64)   // 329.6 Hz
+#define FF4  (229095/64)   // 349.2 Hz
+#define EE4 (242718/64)   // 329.6 Hz
 #define EF4 (257152/64)   // 311.1 Hz
-#define D4  (272386/64)   // 293.7 Hz
+#define DD4  (272386/64)   // 293.7 Hz
 #define DF4 (288600/64)   // 277.2 Hz
-#define C4  (305810/64)   // 261.6 Hz
-#define B3  (324017/64)   // 246.9 Hz
+#define CC4  (305810/64)   // 261.6 Hz
+#define BB3  (324017/64)   // 246.9 Hz
 #define BF3 (343200/64)   // 233.1 Hz
-#define A3  (363636/64)   // 220 Hz
+#define AA3  (363636/64)   // 220 Hz
 #define AF3 (385170/64)   // 207.7 Hz
-#define G3  (408163/64)   // 196 Hz
+#define GG3  (408163/64)   // 196 Hz
 #define GF3 (432432/64)   // 185 Hz
-#define F3  (458190/64)   // 174.6 Hz
-#define E3  (485436/64)   // 164.8 Hz
+#define FF3  (458190/64)   // 174.6 Hz
+#define EE3  (485436/64)   // 164.8 Hz
 #define EF3 (514138/64)   // 155.6 Hz
-#define D3  (544959/64)   // 146.8 Hz
+#define DD3  (544959/64)   // 146.8 Hz
 #define DF3 (577200/64)   // 138.6 Hz
-#define C3  (611620/64)   // 130.8 Hz
+#define CC3  (611620/64)   // 130.8 Hz
 #define SONGSIZE 2
 void playNote(void);
 #define Sixteenth 15000
@@ -86,14 +86,14 @@ uint32_t noteduration;
 
 uint32_t currentnote;
 Note betteroffalonelead[] = {
-    {F5, Eighth/135},{Silent, (Eighth * 14)/135},{E5, Eighth/135},{F5, Eighth/135},{Silent, (Eighth * 15)/135},
-    {F5, Eighth/135},{Silent, (Eighth * 14)/135},{E5, Eighth/135},{F5, Eighth/135},{Silent, (Eighth * 15)/135},
-    {F5, Eighth/135},{Silent, Eighth/135},{F5, Eighth/135},{D5, Eighth/135},{Silent, (Eighth * 7)/135},{C5, Eighth/135},{C6, Eighth/135},{Silent, Sixteenth/135},{C6, Eighth/135},{Silent, Sixteenth/135},{A5, Eighth/135},{F5, Eighth/135},{Silent, Eighth/135},{F5, Eighth/135},{D5, Eighth/135},{Silent, (Eighth * 7)/135},{C5, Eighth/135},{BF5, Eighth/135},{Silent, Sixteenth/135},{BF5, Eighth/135},{Silent, Sixteenth/135},{A5, Eighth/135},
-    {F5, Eighth/135},{Silent, Eighth/135},{F5, Eighth/135},{D5, Eighth/135},{Silent, (Eighth * 7)/135},{C5, Eighth/135},{C6, Eighth/135},{Silent, Sixteenth/135},{C6, Eighth/135},{Silent, Sixteenth/135},{A5, Eighth/135},{F5, Eighth/135},{Silent, Eighth/135},{F5, Eighth/135},{D5, Eighth/135},{Silent, (Eighth * 7)/135},{C5, Eighth/135},{BF5, Eighth/135},{Silent, Sixteenth/135},{BF5, Eighth/135},{Silent, Sixteenth/135},{A5, Eighth/135},
-    {F5, Eighth/135},{Silent, Eighth/135},{F5, Eighth/135},{D5, Eighth/135},{Silent, Eighth/135},{F5, Eighth/135},{Silent, Eighth/135},{F5, Eighth/135},{Silent, Eighth/135},{E5, Eighth/135},{Silent, Eighth/135},{C5, Eighth/135},{C6, Eighth/135},{Silent, Sixteenth/135},{C6, Eighth/135},{Silent, Sixteenth/135},{A5, Eighth/135},{F5, Eighth/135},{Silent, Eighth/135},{F5, Eighth/135},{D5, Eighth/135},{Silent, Eighth/135},{F5, Eighth/135},{Silent, Eighth/135},{F5, Eighth/135},{Silent, Eighth/135},{E5, Eighth/135},{Silent, Eighth/135},{C5, Eighth/135},{BF5, Eighth/135},{Silent, Sixteenth/135},{BF5, Eighth/135},{Silent, Sixteenth/135},{A5, Eighth/135},
-    {F5, Eighth/135},{Silent, Eighth/135},{F5, Eighth/135},{D5, Eighth/135},{Silent, Eighth/135},{F5, Eighth/135},{Silent, Eighth/135},{F5, Eighth/135},{Silent, Eighth/135},{E5, Eighth/135},{Silent, Eighth/135},{C5, Eighth/135},{C6, Eighth/135},{Silent, Sixteenth/135},{C6, Eighth/135},{Silent, Sixteenth/135},{A5, Eighth/135},{F5, Eighth/135},{Silent, Eighth/135},{F5, Eighth/135},{D5, Eighth/135},{Silent, Eighth/135},{F5, Eighth/135},{Silent, Eighth/135},{F5, Eighth/135},{Silent, Eighth/135},{E5, Eighth/135},{Silent, Eighth/135},{C5, Eighth/135},{BF5, Eighth/135},{Silent, Sixteenth/135},{BF5, Eighth/135},{Silent, Sixteenth/135},{A5, Eighth/135},
-    {F5, Eighth/135},{Silent, Eighth/135},{F5, Eighth/135},{D5, Eighth/135},{Silent, Eighth/135},{F5, Eighth/135},{Silent, Eighth/135},{F5, Eighth/135},{Silent, Eighth/135},{E5, Eighth/135},{Silent, Eighth/135},{C5, Eighth/135},{C6, Eighth/135},{Silent, Sixteenth/135},{C6, Eighth/135},{Silent, Sixteenth/135},{A5, Eighth/135},{F5, Eighth/135},{Silent, Eighth/135},{F5, Eighth/135},{D5, Eighth/135},{Silent, Eighth/135},{F5, Eighth/135},{Silent, Eighth/135},{F5, Eighth/135},{Silent, Eighth/135},{E5, Eighth/135},{Silent, Eighth/135},{C5, Eighth/135},{BF5, Eighth/135},{Silent, Sixteenth/135},{BF5, Eighth/135},{Silent, Sixteenth/135},{A5, Eighth/135},
-    {F5, Eighth/135},{Silent, Eighth/135},{F5, Eighth/135},{D5, Eighth/135},{Silent, Eighth/135},{F5, Eighth/135},{Silent, Eighth/135},{F5, Eighth/135},{Silent, Eighth/135},{E5, Eighth/135},{Silent, Eighth/135},{C5, Eighth/135},{C6, Eighth/135},{Silent, Sixteenth/135},{C6, Eighth/135},{Silent, Sixteenth/135},{A5, Eighth/135},{F5, Eighth/135},{Silent, Eighth/135},{F5, Eighth/135},{D5, Eighth/135},{Silent, Eighth/135},{F5, Eighth/135},{Silent, Eighth/135},{F5, Eighth/135},{Silent, Eighth/135},{E5, Eighth/135},{Silent, Eighth/135},{C5, Eighth/135},{BF5, Eighth/135},{Silent, Sixteenth/135},{BF5, Eighth/135},{Silent, Sixteenth/135},{A5, Eighth/135},
+    {FF5, Eighth/135},{Silent, (Eighth * 14)/135},{EE5, Eighth/135},{FF5, Eighth/135},{Silent, (Eighth * 15)/135},
+    {FF5, Eighth/135},{Silent, (Eighth * 14)/135},{EE5, Eighth/135},{FF5, Eighth/135},{Silent, (Eighth * 15)/135},
+    {FF5, Eighth/135},{Silent, Eighth/135},{FF5, Eighth/135},{DD5, Eighth/135},{Silent, (Eighth * 7)/135},{CC5, Eighth/135},{CC6, Eighth/135},{Silent, Sixteenth/135},{CC6, Eighth/135},{Silent, Sixteenth/135},{AA5, Eighth/135},{FF5, Eighth/135},{Silent, Eighth/135},{FF5, Eighth/135},{DD5, Eighth/135},{Silent, (Eighth * 7)/135},{CC5, Eighth/135},{BF5, Eighth/135},{Silent, Sixteenth/135},{BF5, Eighth/135},{Silent, Sixteenth/135},{AA5, Eighth/135},
+    {FF5, Eighth/135},{Silent, Eighth/135},{FF5, Eighth/135},{DD5, Eighth/135},{Silent, (Eighth * 7)/135},{CC5, Eighth/135},{CC6, Eighth/135},{Silent, Sixteenth/135},{CC6, Eighth/135},{Silent, Sixteenth/135},{AA5, Eighth/135},{FF5, Eighth/135},{Silent, Eighth/135},{FF5, Eighth/135},{DD5, Eighth/135},{Silent, (Eighth * 7)/135},{CC5, Eighth/135},{BF5, Eighth/135},{Silent, Sixteenth/135},{BF5, Eighth/135},{Silent, Sixteenth/135},{AA5, Eighth/135},
+    {FF5, Eighth/135},{Silent, Eighth/135},{FF5, Eighth/135},{DD5, Eighth/135},{Silent, Eighth/135},{FF5, Eighth/135},{Silent, Eighth/135},{FF5, Eighth/135},{Silent, Eighth/135},{EE5, Eighth/135},{Silent, Eighth/135},{CC5, Eighth/135},{CC6, Eighth/135},{Silent, Sixteenth/135},{CC6, Eighth/135},{Silent, Sixteenth/135},{AA5, Eighth/135},{FF5, Eighth/135},{Silent, Eighth/135},{FF5, Eighth/135},{DD5, Eighth/135},{Silent, Eighth/135},{FF5, Eighth/135},{Silent, Eighth/135},{FF5, Eighth/135},{Silent, Eighth/135},{EE5, Eighth/135},{Silent, Eighth/135},{CC5, Eighth/135},{BF5, Eighth/135},{Silent, Sixteenth/135},{BF5, Eighth/135},{Silent, Sixteenth/135},{AA5, Eighth/135},
+    {FF5, Eighth/135},{Silent, Eighth/135},{FF5, Eighth/135},{DD5, Eighth/135},{Silent, Eighth/135},{FF5, Eighth/135},{Silent, Eighth/135},{FF5, Eighth/135},{Silent, Eighth/135},{EE5, Eighth/135},{Silent, Eighth/135},{CC5, Eighth/135},{CC6, Eighth/135},{Silent, Sixteenth/135},{CC6, Eighth/135},{Silent, Sixteenth/135},{AA5, Eighth/135},{FF5, Eighth/135},{Silent, Eighth/135},{FF5, Eighth/135},{DD5, Eighth/135},{Silent, Eighth/135},{FF5, Eighth/135},{Silent, Eighth/135},{FF5, Eighth/135},{Silent, Eighth/135},{EE5, Eighth/135},{Silent, Eighth/135},{CC5, Eighth/135},{BF5, Eighth/135},{Silent, Sixteenth/135},{BF5, Eighth/135},{Silent, Sixteenth/135},{AA5, Eighth/135},
+    {FF5, Eighth/135},{Silent, Eighth/135},{FF5, Eighth/135},{DD5, Eighth/135},{Silent, Eighth/135},{FF5, Eighth/135},{Silent, Eighth/135},{FF5, Eighth/135},{Silent, Eighth/135},{EE5, Eighth/135},{Silent, Eighth/135},{CC5, Eighth/135},{CC6, Eighth/135},{Silent, Sixteenth/135},{CC6, Eighth/135},{Silent, Sixteenth/135},{AA5, Eighth/135},{FF5, Eighth/135},{Silent, Eighth/135},{FF5, Eighth/135},{DD5, Eighth/135},{Silent, Eighth/135},{FF5, Eighth/135},{Silent, Eighth/135},{FF5, Eighth/135},{Silent, Eighth/135},{EE5, Eighth/135},{Silent, Eighth/135},{CC5, Eighth/135},{BF5, Eighth/135},{Silent, Sixteenth/135},{BF5, Eighth/135},{Silent, Sixteenth/135},{AA5, Eighth/135},
+    {FF5, Eighth/135},{Silent, Eighth/135},{FF5, Eighth/135},{DD5, Eighth/135},{Silent, Eighth/135},{FF5, Eighth/135},{Silent, Eighth/135},{FF5, Eighth/135},{Silent, Eighth/135},{EE5, Eighth/135},{Silent, Eighth/135},{CC5, Eighth/135},{CC6, Eighth/135},{Silent, Sixteenth/135},{CC6, Eighth/135},{Silent, Sixteenth/135},{AA5, Eighth/135},{FF5, Eighth/135},{Silent, Eighth/135},{FF5, Eighth/135},{DD5, Eighth/135},{Silent, Eighth/135},{FF5, Eighth/135},{Silent, Eighth/135},{FF5, Eighth/135},{Silent, Eighth/135},{EE5, Eighth/135},{Silent, Eighth/135},{CC5, Eighth/135},{BF5, Eighth/135},{Silent, Sixteenth/135},{BF5, Eighth/135},{Silent, Sixteenth/135},{AA5, Eighth/135},
 
 };
 
@@ -102,14 +102,14 @@ Note betteroffalonebass[] = {
 };
 
 Note toujourlead[] = {
-    {D4, Whole/128},{GF4, Whole/128},{B4, Whole/128},{G4, Whole/128},
-    {D4, (Quarter * 2)/128},{GF4, Eighth/128},{Silent, Eighth/128},{GF4, Eighth/128},{D5, Eighth/128},{DF5, Quarter/128},{Silent, (Quarter * 5)/128},{B4, Eighth/128},{Silent, Eighth/128},{B4, Eighth/128},{A4, Eighth/128},{B4, Quarter/128},{Silent, (Quarter * 5)/128},
-    {GF4, Eighth/128},{Silent, Eighth/128},{GF4, Eighth/128},{D5, Eighth/128},{DF5, Quarter/128},{Silent, (Quarter * 5)/128},{B4, Eighth/128},{Silent, Eighth/128},{B4, Eighth/128},{A4, Eighth/128},{B4, Quarter/128},{Silent, Quarter/128},{B4, Quarter/128},{Silent, Quarter/128},{B4, Quarter/128},{A4, Eighth/128},{B4, Quarter/128},{A4, Eighth/128},
-    {GF4, Quarter/128},{Silent, Quarter/128},{GF4, Eighth/128},{Silent, Eighth/128},{GF4, Eighth/128},{D5, Eighth/128},{DF5, Quarter/128},{Silent, Quarter/128},{DF5, Eighth/128},{Silent, Eighth/128},{DF5, Eighth/128},{D5, Eighth/128},{B4, Quarter/128},{Silent, Quarter/128},{B4, Eighth/128},{Silent, Eighth/128},{B4, Eighth/128},{A4, Eighth/128},{B4, Eighth/128},{Silent, Eighth/128},{B4, Eighth/128},{Silent, Eighth/128},{B4, Eighth/128},{A4, Eighth/128},{B4, Eighth/128},{A4, Eighth/128},
-    {GF4, Quarter/128},{Silent, Quarter/128},{GF4, Eighth/128},{Silent, Eighth/128},{GF4, Eighth/128},{D5, Eighth/128},{DF5, Quarter/128},{Silent, Quarter/128},{DF5, Eighth/128},{Silent, Eighth/128},{DF5, Eighth/128},{D5, Eighth/128},{B4, Quarter/128},{Silent, Quarter/128},{B4, Eighth/128},{Silent, Eighth/128},{B4, Eighth/128},{A4, Eighth/128},{B4, Eighth/128},{Silent, Eighth/128},{B4, Eighth/128},{Silent, Eighth/128},{B4, Eighth/128},{A4, Eighth/128},{B4, Eighth/128},{A4, Eighth/128},
-    {GF4, Quarter/128},{Silent, Quarter/128},{GF4, Eighth/128},{Silent, Eighth/128},{GF4, Eighth/128},{D5, Eighth/128},{DF5, Quarter/128},{Silent, Quarter/128},{DF5, Eighth/128},{Silent, Eighth/128},{DF5, Eighth/128},{D5, Eighth/128},{B4, Quarter/128},{Silent, Quarter/128},{B4, Eighth/128},{Silent, Eighth/128},{B4, Eighth/128},{A4, Eighth/128},{B4, Eighth/128},{Silent, Eighth/128},{B4, Eighth/128},{Silent, Eighth/128},{B4, Eighth/128},{A4, Eighth/128},{B4, Eighth/128},{A4, Eighth/128},
-    {GF4, Quarter/128},{Silent, Quarter/128},{GF4, Eighth/128},{Silent, Eighth/128},{GF4, Eighth/128},{D5, Eighth/128},{DF5, Quarter/128},{Silent, Quarter/128},{DF5, Eighth/128},{Silent, Eighth/128},{DF5, Eighth/128},{D5, Eighth/128},{B4, Quarter/128},{Silent, Quarter/128},{B4, Eighth/128},{Silent, Eighth/128},{B4, Eighth/128},{A4, Eighth/128},{B4, Eighth/128},{Silent, Eighth/128},{B4, Eighth/128},{Silent, Eighth/128},{B4, Eighth/128},{A4, Eighth/128},{B4, Eighth/128},{A4, Eighth/128},
-    {GF4, Quarter/128},{Silent, Quarter/128},{GF4, Eighth/128},{Silent, Eighth/128},{GF4, Eighth/128},{D5, Eighth/128},{DF5, Quarter/128},{Silent, Quarter/128},{DF5, Eighth/128},{Silent, Eighth/128},{DF5, Eighth/128},{D5, Eighth/128},{B4, Quarter/128},{Silent, Quarter/128},{B4, Eighth/128},{Silent, Eighth/128},{B4, Eighth/128},{A4, Eighth/128},{B4, Quarter/128},{Silent, (Quarter * 3)/128}
+    {DD4, Whole/128},{GF4, Whole/128},{BB4, Whole/128},{GG4, Whole/128},
+    {DD4, (Quarter * 2)/128},{GF4, Eighth/128},{Silent, Eighth/128},{GF4, Eighth/128},{DD5, Eighth/128},{DF5, Quarter/128},{Silent, (Quarter * 5)/128},{BB4, Eighth/128},{Silent, Eighth/128},{BB4, Eighth/128},{AA4, Eighth/128},{BB4, Quarter/128},{Silent, (Quarter * 5)/128},
+    {GF4, Eighth/128},{Silent, Eighth/128},{GF4, Eighth/128},{DD5, Eighth/128},{DF5, Quarter/128},{Silent, (Quarter * 5)/128},{BB4, Eighth/128},{Silent, Eighth/128},{BB4, Eighth/128},{AA4, Eighth/128},{BB4, Quarter/128},{Silent, Quarter/128},{BB4, Quarter/128},{Silent, Quarter/128},{BB4, Quarter/128},{AA4, Eighth/128},{BB4, Quarter/128},{AA4, Eighth/128},
+    {GF4, Quarter/128},{Silent, Quarter/128},{GF4, Eighth/128},{Silent, Eighth/128},{GF4, Eighth/128},{DD5, Eighth/128},{DF5, Quarter/128},{Silent, Quarter/128},{DF5, Eighth/128},{Silent, Eighth/128},{DF5, Eighth/128},{DD5, Eighth/128},{BB4, Quarter/128},{Silent, Quarter/128},{BB4, Eighth/128},{Silent, Eighth/128},{BB4, Eighth/128},{AA4, Eighth/128},{BB4, Eighth/128},{Silent, Eighth/128},{BB4, Eighth/128},{Silent, Eighth/128},{BB4, Eighth/128},{AA4, Eighth/128},{BB4, Eighth/128},{AA4, Eighth/128},
+    {GF4, Quarter/128},{Silent, Quarter/128},{GF4, Eighth/128},{Silent, Eighth/128},{GF4, Eighth/128},{DD5, Eighth/128},{DF5, Quarter/128},{Silent, Quarter/128},{DF5, Eighth/128},{Silent, Eighth/128},{DF5, Eighth/128},{DD5, Eighth/128},{BB4, Quarter/128},{Silent, Quarter/128},{BB4, Eighth/128},{Silent, Eighth/128},{BB4, Eighth/128},{AA4, Eighth/128},{BB4, Eighth/128},{Silent, Eighth/128},{BB4, Eighth/128},{Silent, Eighth/128},{BB4, Eighth/128},{AA4, Eighth/128},{BB4, Eighth/128},{AA4, Eighth/128},
+    {GF4, Quarter/128},{Silent, Quarter/128},{GF4, Eighth/128},{Silent, Eighth/128},{GF4, Eighth/128},{DD5, Eighth/128},{DF5, Quarter/128},{Silent, Quarter/128},{DF5, Eighth/128},{Silent, Eighth/128},{DF5, Eighth/128},{DD5, Eighth/128},{BB4, Quarter/128},{Silent, Quarter/128},{BB4, Eighth/128},{Silent, Eighth/128},{BB4, Eighth/128},{AA4, Eighth/128},{BB4, Eighth/128},{Silent, Eighth/128},{BB4, Eighth/128},{Silent, Eighth/128},{BB4, Eighth/128},{AA4, Eighth/128},{BB4, Eighth/128},{AA4, Eighth/128},
+    {GF4, Quarter/128},{Silent, Quarter/128},{GF4, Eighth/128},{Silent, Eighth/128},{GF4, Eighth/128},{DD5, Eighth/128},{DF5, Quarter/128},{Silent, Quarter/128},{DF5, Eighth/128},{Silent, Eighth/128},{DF5, Eighth/128},{DD5, Eighth/128},{BB4, Quarter/128},{Silent, Quarter/128},{BB4, Eighth/128},{Silent, Eighth/128},{BB4, Eighth/128},{AA4, Eighth/128},{BB4, Eighth/128},{Silent, Eighth/128},{BB4, Eighth/128},{Silent, Eighth/128},{BB4, Eighth/128},{AA4, Eighth/128},{BB4, Eighth/128},{AA4, Eighth/128},
+    {GF4, Quarter/128},{Silent, Quarter/128},{GF4, Eighth/128},{Silent, Eighth/128},{GF4, Eighth/128},{DD5, Eighth/128},{DF5, Quarter/128},{Silent, Quarter/128},{DF5, Eighth/128},{Silent, Eighth/128},{DF5, Eighth/128},{DD5, Eighth/128},{BB4, Quarter/128},{Silent, Quarter/128},{BB4, Eighth/128},{Silent, Eighth/128},{BB4, Eighth/128},{AA4, Eighth/128},{BB4, Quarter/128},{Silent, (Quarter * 3)/128}
 
 };
 
